402778974c9dc97075cca6affa08be342e184275
ayman blidi <blidiayman47@gmail.com>
2025-02-16 11:08:31 +0100

    creat thr models

--- backend/api/app/models.py
+++ backend/api/app/models.py
@@ -1,67 +1,74 @@
 from django.db import models
 
-# Create your models here.
-
-class Products(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, default='No description available')
-    price = models.FloatField()
-    discount = models.FloatField()
-    image_url = models.ImageField(upload_to='images/products/')
-    category = models.OneToOneField('category', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
     image_url = models.ImageField(upload_to='images/category/')
 
     def __str__(self):
         return self.name
     
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, default='No description available')
+    price = models.FloatField()
+    discount = models.FloatField()
+    image_url = models.ImageField(upload_to='images/products/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Delevry(models.Model):
+    # i will modify this model like a user model
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
+
+class Address(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    lang = models.FloatField()
+    lat = models.FloatField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name, 'of', self.user
+    
 class Orders(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    delivery = models.BooleanField(default=False)
+    delivery = models.ForeignKey(Delevry, on_delete=models.CASCADE)
     total_price = models.FloatField()
     quantity = models.IntegerField()
-    address = models.CharField(max_length=1000)
-    status = models.CharField(max_length=100, default='Pending')
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    status = models.TextChoices(['pending','delivered','3','4'])
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.product.name
+        return self.user
     
-class address(models.Model):
-    address = models.CharField(max_length=1000)
-    phone = models.CharField(max_length=100)
-    email = models.EmailField()
 
-    def __str__(self):
-        return self.email
 
 class Cart(models.Model):
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     total_price = models.FloatField()
-    address = models.CharField(max_length=1000)
-    phone = models.CharField(max_length=100)
-    email = models.EmailField()
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.product.name
 
-class coupon(models.Model):
-    code = models.CharField(max_length=100)
-    discount = models.FloatField()
-
-    def __str__(self):
-        return self.code
-
-class delevry(models.Model):
+class Coupon(models.Model):
     name = models.CharField(max_length=100)
-    price = models.FloatField()
+    discount = models.FloatField()
+    quantity = models.IntegerField()
+    dateEx = models.DateField()
 
     def __str__(self):
         return self.name
+
+
