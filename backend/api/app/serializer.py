499e58fc07c2ee1d273878cc7536ffe010378268
ayman blidi <blidiayman47@gmail.com>
2025-02-20 15:18:54 +0100

    show category details in products responses

--- backend/api/app/serializer.py
+++ backend/api/app/serializer.py
@@ -1,14 +1,21 @@
-from .models import Product
+from .models import Product, Category
 from rest_framework import serializers
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
     is_favorite = serializers.SerializerMethodField()
+    category = CategorySerializer()
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'discount', 'image_url', 'category', 'is_favorite']  
+        fields = ['id', 'name', 'description', 'image_url', 'price', 'discount', 'is_favorite', 'category']  
 
     def get_is_favorite(self, obj):
         user = self.context.get('request').user
         return user in obj.favorited_by.all()
+    
+    
